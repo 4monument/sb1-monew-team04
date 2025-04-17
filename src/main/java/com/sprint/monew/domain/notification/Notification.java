4d4a955ab9066b2a90dbdf32a5d06f89e85d24b3
yudility <yudility@gmail.com>
2025-04-17 17:21:35 +0900

    feat: 알림 Entity, Dto, CursorPageDto 구현

--- src/main/java/com/sprint/monew/domain/notification/Notification.java
+++ src/main/java/com/sprint/monew/domain/notification/Notification.java
@@ -1,5 +1,55 @@
 package com.sprint.monew.domain.notification;
 
+
+import com.sprint.monew.domain.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "notifications")
+@Entity
+@NoArgsConstructor
 public class Notification {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  private UUID resourceId;
+
+  // INTEREST, COMMENT
+  private ResourceType resourceType;
+
+  private String content;
+
+  private Instant createdAt;
+
+  private Instant updatedAt;
+
+  private boolean confirmed;
+
+  public Notification(User user, UUID resourceId, ResourceType resourceType, String content) {
+    this.user = user;
+    this.resourceId = resourceId;
+    this.resourceType = resourceType;
+    this.content = content;
+    this.createdAt = Instant.now();
+    this.updatedAt = createdAt;
+    this.confirmed = false;
+  }
 }
