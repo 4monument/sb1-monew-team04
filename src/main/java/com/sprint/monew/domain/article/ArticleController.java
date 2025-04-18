58fd34937cd68c6618b88d715ae87bd04ebedcbc
Oince <hky0105@naver.com>
2025-04-18 13:07:52 +0900

    feat: ArticleService 추가

--- src/main/java/com/sprint/monew/domain/article/ArticleController.java
+++ src/main/java/com/sprint/monew/domain/article/ArticleController.java
@@ -1,9 +1,7 @@
 package com.sprint.monew.domain.article;
 
-import com.sprint.monew.domain.article.dto.ArticleDto;
 import com.sprint.monew.domain.article.dto.ArticleRestoreResultDto;
 import com.sprint.monew.domain.article.dto.ArticleViewDto;
-import com.sprint.monew.domain.article.dto.request.ArticleRequest;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -12,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -30,10 +27,11 @@ public class ArticleController {
 
   @PostMapping("/{id}/article-views")
   public ResponseEntity<ArticleViewDto> registerArticleView(
-      @PathVariable String id,
+      @PathVariable UUID articleId,
       @RequestHeader("Monew-Request-User-ID") UUID userId
       ) {
-    return null;
+    ArticleViewDto articleViewDto = articleService.registerArticleView(articleId, userId);
+    return ResponseEntity.ok(articleViewDto);
   }
 
   //CursorPageResponse가 아직 없어서 일단 주석으로 처리했습니다.
@@ -45,7 +43,11 @@ public class ArticleController {
 //      @RequestParam int limit,
 //      @RequestHeader("Monew-Request-User-ID") UUID userId
 //  ) {
-//    return null;
+//    //임시 코드
+//    PageRequest pageRequest = PageRequest.of(0, limit);
+//    CursorPageResponse<ArticleDto> response = articleService.getArticles(articleRequest,
+//        pageRequest);
+//    return ResponseEntity.ok(response);
 //  }
 
   @GetMapping("/restore")
