6dbc9d1bff24d3369fa5aeab55ec5009fa605b1e
yudility <yudility@gmail.com>
2025-04-18 10:35:50 +0900

    fix: 관심사 DTO record로 변경

--- src/main/java/com/sprint/monew/domain/interest/InterestDto.java
+++ src/main/java/com/sprint/monew/domain/interest/InterestDto.java
@@ -2,16 +2,14 @@ package com.sprint.monew.domain.interest;
 
 import java.util.List;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
-public class InterestDto {
-
-  private UUID id;
-  private String name;
-  private List<String> keywords;
-  private int subscriberCount;
-  private boolean subscribedByMe;
+public record InterestDto(
+    UUID id,
+    String name,
+    List<String> keywords,
+    int subscriberCount,
+    boolean subscribedByMe
+) {
 
   public static InterestDto from(Interest interest, int subscriberCount, boolean subscribedByMe) {
     return new InterestDto(
--- src/main/java/com/sprint/monew/domain/interest/SubscriptionDto.java
+++ src/main/java/com/sprint/monew/domain/interest/SubscriptionDto.java
@@ -3,18 +3,17 @@ package com.sprint.monew.domain.interest;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
-public class SubscriptionDto {
-  private UUID id; // 항상 null로 반환됨. why?
-  private UUID interestId;
-  private String interestName;
-  private List<String> interestKeywords;
-  private int interestSubscriberCount;
-  private Instant createdAt;
+public record SubscriptionDto(
+    UUID id, // 항상 null로 반환됨. why?
+    UUID interestId,
+    String interestName,
+    List<String> interestKeywords,
+    int interestSubscriberCount,
+    Instant createdAt
+) {
 
-  public static SubscriptionDto from(Interest interest, int subscriberCount, Instant createdAt ) {
+  public static SubscriptionDto from(Interest interest, int subscriberCount, Instant createdAt) {
     return new SubscriptionDto(
         null,
         interest.getId(),
