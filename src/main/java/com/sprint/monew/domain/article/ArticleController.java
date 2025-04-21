9c320639cabcb780f43b149097df87890138f11f
Oince <hky0105@naver.com>
2025-04-21 11:27:17 +0900

    refactor: 주석 처리 해제

--- src/main/java/com/sprint/monew/domain/article/ArticleController.java
+++ src/main/java/com/sprint/monew/domain/article/ArticleController.java
@@ -1,15 +1,20 @@
 package com.sprint.monew.domain.article;
 
+import com.sprint.monew.common.util.CursorPageResponseDto;
+import com.sprint.monew.domain.article.dto.ArticleDto;
 import com.sprint.monew.domain.article.dto.ArticleRestoreResultDto;
 import com.sprint.monew.domain.article.dto.ArticleViewDto;
+import com.sprint.monew.domain.article.dto.request.ArticleRequest;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -34,21 +39,20 @@ public class ArticleController {
     return ResponseEntity.ok(articleViewDto);
   }
 
-  //CursorPageResponse가 아직 없어서 일단 주석으로 처리했습니다.
-//  @GetMapping
-//  public ResponseEntity<CursorPageResponse<ArticleDto>> getArticles(
-//      @ModelAttribute ArticleRequest articleRequest,
-//      @RequestParam String orderBy,
-//      @RequestParam String direction,
-//      @RequestParam int limit,
-//      @RequestHeader("Monew-Request-User-ID") UUID userId
-//  ) {
-//    //임시 코드
-//    PageRequest pageRequest = PageRequest.of(0, limit);
-//    CursorPageResponse<ArticleDto> response = articleService.getArticles(articleRequest,
-//        pageRequest);
-//    return ResponseEntity.ok(response);
-//  }
+  @GetMapping
+  public ResponseEntity<CursorPageResponseDto<ArticleDto>> getArticles(
+      @ModelAttribute ArticleRequest articleRequest,
+      @RequestParam String orderBy,
+      @RequestParam String direction,
+      @RequestParam int limit,
+      @RequestHeader("Monew-Request-User-ID") UUID userId
+  ) {
+    //임시 코드
+    PageRequest pageRequest = PageRequest.of(0, limit);
+    CursorPageResponseDto<ArticleDto> response = articleService.getArticles(articleRequest,
+        pageRequest);
+    return ResponseEntity.ok(response);
+  }
 
   @GetMapping("/restore")
   public ResponseEntity<List<ArticleRestoreResultDto>> restoreArticles(
