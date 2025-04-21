537cb02b5fd637e9b2d8e35bce0e13ac531b6b64
이요한 <icb1696@naver.com>
2025-04-21 16:15:03 +0900

    [Feature] : flatFileItemWriter 골격 완성

--- src/main/java/com/sprint/monew/common/batch/S3BackupBatch.java
+++ src/main/java/com/sprint/monew/common/batch/S3BackupBatch.java
@@ -1,16 +1,34 @@
 package com.sprint.monew.common.batch;
 
 import com.sprint.monew.domain.article.Article;
+import io.awspring.cloud.s3.S3Resource;
+import jakarta.annotation.Resource;
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,26 +36,30 @@ public class S3BackupBatch {
 
   private final EntityManagerFactory emf;
   private final JobRepository jobRepository;
+  private final PlatformTransactionManager transactionManager;
 
+  @Resource(name = "articleS3Resource")
+  private final S3Resource articleS3Resource;
+
+  @Primary
   @Bean
   public Job s3BackupJob() {
-    return null;
+    return new JobBuilder("s3BackupJob", jobRepository)
+        .incrementer(new RunIdIncrementer())
+        .start(s3BackupStep())
+        .build();
   }
-
 
   @Bean
   public Step s3BackupStep() {
-
-    new StepBuilder("s3BackupStep", jobRepository)
-        .<Article, Article>chunk(10)
+    return new StepBuilder("s3BackupStep", jobRepository)
+        .<Article, Article>chunk(2, transactionManager)
         .reader(jpaPagingItemReader())
-        .writer(null)
+        .writer(flatFileItemWriter())
         .build();
-
-    return null;
   }
 
-
+  // ItemReader : jpaPagingItemReader 기본 구현체 사용
   @Bean
   public JpaPagingItemReader<Article> jpaPagingItemReader(){
     String query = "SELECT a FROM Article a";
@@ -47,5 +69,31 @@ public class S3BackupBatch {
         .entityManagerFactory(emf)
         .queryString(query)
         .build();
+  }
+
+  // ItemWriter : S3에 백업하기 위한 FlatFileItemWriter 구현
+  @Bean
+  public FlatFileItemWriter<Article> flatFileItemWriter() {
+    // 문제는
+    return new FlatFileItemWriterBuilder<Article>()
+        .name("s3BackupWriter")
+        .resource(articleS3Resource)
+        .append(true)
+        .delimited().delimiter("|")  // 나중에 @Value로 바꾸기
+        .names(getFieldNames())
+        .build();
+  }
+
+  // 하드코딩 해결 나중에 생각하기
+  private String[] getFieldNames() {
+    return new String[]{
+        "id",
+        "source",
+        "sourceUrl",
+        "title",
+        "pulishDate",
+        "summary",
+        "deleted"
+    };
   }
 }
--- src/main/java/com/sprint/monew/global/config/S3Config.java
+++ src/main/java/com/sprint/monew/global/config/S3Config.java
@@ -1,9 +1,15 @@
 package com.sprint.monew.global.config;
 
+import io.awspring.cloud.s3.S3Operations;
+import io.awspring.cloud.s3.S3OutputStreamProvider;
+import io.awspring.cloud.s3.S3Resource;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -15,6 +21,8 @@ import software.amazon.awssdk.services.s3.S3Client;
 public class S3Config {
 
   private final S3ConfigProperties s3Properties;
+  private final S3OutputStreamProvider s3OutputStreamProvider;
+  //private final S3Operations s3Operations;
 
   @Bean
   public S3Client s3Client() {
@@ -27,8 +35,22 @@ public class S3Config {
         .build();
   }
 
+  // 멀티파트, 메모리 완화 기능 위해 S3Resource.create 사용 및 S3Resource로 반환
+  @Bean(name = "articleS3Resource")
+  public S3Resource articleS3Resource() {
+    //s3Operations.createResource(s3Properties.bucketName(), ...)
+    String location = "s3://" + s3Properties.bucketName();
+    return S3Resource.create(location, s3Client(), s3OutputStreamProvider);
+  }
+
   private AwsBasicCredentials getAwsBasicCredentials() {
     return AwsBasicCredentials.create(s3Properties.accessKey(),
         s3Properties.secretKey());
   }
+
+//  @Bean
+//  public Resource resourceLoader(ResourceLoader resourceLoader) {
+//    String location = "s3://" + s3Properties.bucketName();
+//    return resourceLoader.getResource(location);
+//  }
 }
