53118e814809d7553a559a6a8a01694264ae24fd
yudility <yudility@gmail.com>
2025-04-22 14:30:20 +0900

    feat: 기사 조회 내역 엔티티 구현

---
+++ src/main/java/com/sprint/monew/domain/article/articleveiw/ArticleViewKey.java
@@ -0,0 +1,19 @@
+package com.sprint.monew.domain.article.articleveiw;
+
+import jakarta.persistence.Embeddable;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleViewKey {
+
+  private UUID articleId;
+  private UUID interestId;
+}
---
+++ src/main/java/com/sprint/monew/domain/article/articleveiw/ArticleViewRepository.java
@@ -0,0 +1,7 @@
+package com.sprint.monew.domain.article.articleveiw;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleViewRepository extends JpaRepository<ArticleView, ArticleViewKey> {
+
+}
