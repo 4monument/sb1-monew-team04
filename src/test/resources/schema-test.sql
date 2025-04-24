af3ba261ac072108e85d83564a75711c5340a78f
janghoosa <janghoosa@gmail.com>
2025-04-24 16:30:08 +0900

    test: 스키마 업데이트

--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "users"
 (
-    "id"         UUID         PRIMARY KEY,
+    "id"         UUID PRIMARY KEY,
     "email"      VARCHAR(255) NOT NULL UNIQUE,
     "nickname"   VARCHAR(255) NOT NULL,
     "password"   VARCHAR(255) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE "users"
 
 CREATE TABLE "articles"
 (
-    "id"           UUID          PRIMARY KEY,
+    "id"           UUID PRIMARY KEY,
     "source"       VARCHAR(255)  NOT NULL,
     "source_url"   VARCHAR(2048) NOT NULL UNIQUE,
     "title"        VARCHAR(255)  NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE "articles"
 
 CREATE TABLE "comments"
 (
-    "id"         UUID         PRIMARY KEY,
+    "id"         UUID PRIMARY KEY,
     "user_id"    UUID         NOT NULL,
     "article_id" UUID         NOT NULL,
     "content"    VARCHAR(255) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE "comments"
 
 CREATE TABLE "notifications"
 (
-    "id"            UUID         PRIMARY KEY,
+    "id"            UUID PRIMARY KEY,
     "user_id"       UUID         NOT NULL,
     "resource_id"   UUID         NOT NULL,
     "resource_type" VARCHAR(50)  NOT NULL,
@@ -43,31 +43,33 @@ CREATE TABLE "notifications"
 
 CREATE TABLE "interests"
 (
-    "id"       UUID        PRIMARY KEY,
-    "name"     VARCHAR(50) NOT NULL UNIQUE,
-    "keywords" JSONB      NULL,
-    "created_at"  TIMESTAMPTZ NOT NULL DEFAULT now()
+    "id"         UUID PRIMARY KEY,
+    "name"       VARCHAR(50) NOT NULL UNIQUE,
+    "keywords"   JSONB       NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE "users_interests"
 (
+    "id"          UUID PRIMARY KEY,
     "user_id"     UUID,
     "interest_id" UUID,
     "created_at"  TIMESTAMPTZ NOT NULL DEFAULT now(),
-    PRIMARY KEY (user_id, interest_id)
+    UNIQUE (user_id, interest_id)
 );
 
 CREATE TABLE "articles_views"
 (
+    "id"         UUID PRIMARY KEY,
     "user_id"    UUID,
     "article_id" UUID,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-    PRIMARY KEY (user_id, article_id)
+    UNIQUE (user_id, article_id)
 );
 
 CREATE TABLE "likes"
 (
-    "id"         UUID        PRIMARY KEY,
+    "id"         UUID PRIMARY KEY,
     "comment_id" UUID        NOT NULL,
     "user_id"    UUID        NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT now()
@@ -75,9 +77,10 @@ CREATE TABLE "likes"
 
 CREATE TABLE "articles_interests"
 (
+    "id"          UUID PRIMARY KEY,
     "article_id"  UUID NOT NULL,
     "interest_id" UUID NOT NULL,
-    Primary Key (article_id, interest_id)
+    UNIQUE (article_id, interest_id)
 );
 
 ALTER TABLE "comments"
@@ -145,4 +148,206 @@ ALTER TABLE "articles_interests"
         FOREIGN KEY ("interest_id")
             REFERENCES "interests" ("id")
             ON DELETE CASCADE;
+
+
+
+-- we don't know how to generate root <with-no-name> (class Root) :(
+
+comment on database postgres is 'default administrative connection database';
+
+create sequence batch_step_execution_seq;
+
+alter sequence batch_step_execution_seq owner to monew;
+
+create sequence batch_job_execution_seq;
+
+alter sequence batch_job_execution_seq owner to monew;
+
+create sequence batch_job_seq;
+
+alter sequence batch_job_seq owner to monew;
+
+create table batch_job_instance
+(
+    job_instance_id bigint       not null
+        primary key,
+    version         bigint,
+    job_name        varchar(100) not null,
+    job_key         varchar(32)  not null,
+    constraint job_inst_un
+        unique (job_name, job_key)
+);
+
+alter table batch_job_instance
+    owner to monew;
+
+create table batch_job_execution
+(
+    job_execution_id bigint    not null
+        primary key,
+    version          bigint,
+    job_instance_id  bigint    not null
+        constraint job_inst_exec_fk
+            references batch_job_instance,
+    create_time      timestamp not null,
+    start_time       timestamp,
+    end_time         timestamp,
+    status           varchar(10),
+    exit_code        varchar(2500),
+    exit_message     varchar(2500),
+    last_updated     timestamp
+);
+
+alter table batch_job_execution
+    owner to monew;
+
+create table batch_job_execution_params
+(
+    job_execution_id bigint       not null
+        constraint job_exec_params_fk
+            references batch_job_execution,
+    parameter_name   varchar(100) not null,
+    parameter_type   varchar(100) not null,
+    parameter_value  varchar(2500),
+    identifying      char         not null
+);
+
+alter table batch_job_execution_params
+    owner to monew;
+
+create table batch_step_execution
+(
+    step_execution_id  bigint       not null
+        primary key,
+    version            bigint       not null,
+    step_name          varchar(100) not null,
+    job_execution_id   bigint       not null
+        constraint job_exec_step_fk
+            references batch_job_execution,
+    create_time        timestamp    not null,
+    start_time         timestamp,
+    end_time           timestamp,
+    status             varchar(10),
+    commit_count       bigint,
+    read_count         bigint,
+    filter_count       bigint,
+    write_count        bigint,
+    read_skip_count    bigint,
+    write_skip_count   bigint,
+    process_skip_count bigint,
+    rollback_count     bigint,
+    exit_code          varchar(2500),
+    exit_message       varchar(2500),
+    last_updated       timestamp
+);
+
+alter table batch_step_execution
+    owner to monew;
+
+create table batch_step_execution_context
+(
+    step_execution_id  bigint        not null
+        primary key
+        constraint step_exec_ctx_fk
+            references batch_step_execution,
+    short_context      varchar(2500) not null,
+    serialized_context text
+);
+
+alter table batch_step_execution_context
+    owner to monew;
+
+create table batch_job_execution_context
+(
+    job_execution_id   bigint        not null
+        primary key
+        constraint job_exec_ctx_fk
+            references batch_job_execution,
+    short_context      varchar(2500) not null,
+    serialized_context text
+);
+
+alter table batch_job_execution_context
+    owner to monew;
+
+create table articles
+(
+    deleted      boolean default false       not null,
+    publish_date timestamp(6) with time zone not null,
+    id           uuid                        not null
+        primary key,
+    source_url   varchar(2048)               not null
+        unique,
+    source       varchar(255)                not null,
+    summary      varchar(255)                not null,
+    title        varchar(255)                not null
+);
+
+alter table articles
+    owner to monew;
+
+create table interests
+(
+    id       uuid         not null
+        primary key,
+    keywords jsonb,
+    name     varchar(255) not null
+);
+
+alter table interests
+    owner to monew;
+
+create table users
+(
+    deleted    boolean default false    not null,
+    created_at timestamp with time zone not null,
+    id         uuid                     not null
+        primary key,
+    email      varchar(255)             not null
+        unique,
+    nickname   varchar(255)             not null,
+    password   varchar(255)             not null
+);
+
+alter table users
+    owner to monew;
+
+create table comments
+(
+    deleted    boolean default false       not null,
+    created_at timestamp(6) with time zone not null,
+    article_id uuid
+        constraint fkk4ib6syde10dalk7r7xdl0m5p
+            references articles,
+    id         uuid                        not null
+        primary key,
+    user_id    uuid
+        constraint fk8omq0tc18jd43bu5tjh6jvraq
+            references users,
+    content    varchar(255)                not null
+);
+
+alter table comments
+    owner to monew;
+
+create table notifications
+(
+    confirmed     boolean                     not null,
+    created_at    timestamp(6) with time zone not null,
+    updated_at    timestamp(6) with time zone not null,
+    id            uuid                        not null
+        primary key,
+    resource_id   uuid                        not null,
+    user_id       uuid                        not null
+        constraint fk9y21adhxn0ayjhfocscqox7bh
+            references users,
+    content       varchar(255)                not null,
+    resource_type varchar(255)                not null
+        constraint notifications_resource_type_check
+            check ((resource_type)::text = ANY
+                   ((ARRAY ['INTEREST'::character varying, 'COMMENT'::character varying])::text[]))
+);
+
+alter table notifications
+    owner to monew;
 
