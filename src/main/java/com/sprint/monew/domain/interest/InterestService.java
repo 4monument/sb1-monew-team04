4685d348a3b4c47c011f3408972ce403b2b5c852
yudility <yudility@gmail.com>
2025-04-24 16:41:43 +0900

    refactor: 문자열 유사도 검사 로직 클래스로 분리

--- src/main/java/com/sprint/monew/domain/interest/InterestService.java
+++ src/main/java/com/sprint/monew/domain/interest/InterestService.java
@@ -1,5 +1,7 @@
 package com.sprint.monew.domain.interest;
 
+import static com.sprint.monew.common.util.SimilarityCalculator.calculateSimilarity;
+
 import com.sprint.monew.common.util.CursorPageResponseDto;
 import com.sprint.monew.domain.interest.dto.InterestCreateRequest;
 import com.sprint.monew.domain.interest.dto.InterestDto;
@@ -134,13 +136,8 @@ public class InterestService {
 
     Interest interest = new Interest(request.name(), request.keywords());
 
-    boolean existsSimilarName = interestRepository.existsByName(request.name());
-
-    if (existsSimilarName) {
-      throw new IllegalArgumentException("동일한 이름의 관심사가 이미 존재합니다.");
-    }
-
     //findAll()해서 매번 다 비교하면 너무 오래걸리지 않을까?
+    //그렇다고 키워드 하나씩 검사해서 가져오는건 무리가 있을 것 같음. -> QueryDSL 적용할 때 고려해보자.
     List<Interest> allInterests = interestRepository.findAll();
 
     for (Interest i : allInterests) {
@@ -228,51 +225,4 @@ public class InterestService {
             : subscriptionRepository.existsByUserIdAndInterestId(user.getId(), interestId)
     );
   }
-
-  //두 문자열 간 유사도 검사
-  private double calculateSimilarity(String str1, String str2) {
-    if (str1.equals(str2)) {
-      return 1.0;
-    }
-
-    if (str1 == null || str2 == null || str1.isEmpty() || str2.isEmpty()) {
-      return 0.0;
-    }
-
-    int distance = levenshteinDistance(str1, str2);
-
-    // 최대 가능한 거리 (더 긴 문자열의 길이)
-    int maxLength = Math.max(str1.length(), str2.length());
-
-    //유사도 계산
-    return 1.0 - ((double) distance / maxLength);
-  }
-
-  /**
-   * 레벤슈타인 거리를 계산 한 문자열에서 다른 문자열로 변환하는 데 필요한 최소 편집 연산(삽입, 삭제, 대체)의 수.
-   */
-  private int levenshteinDistance(String str1, String str2) {
-    int[][] dp = new int[str1.length() + 1][str2.length() + 1];
-
-    for (int i = 0; i <= str1.length(); i++) {
-      dp[i][0] = i;
-    }
-
-    for (int j = 0; j <= str2.length(); j++) {
-      dp[0][j] = j;
-    }
-
-    for (int i = 1; i <= str1.length(); i++) {
-      for (int j = 1; j <= str2.length(); j++) {
-        int cost = (str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0 : 1;
-        dp[i][j] = Math.min(
-            Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1),
-            dp[i - 1][j - 1] + cost
-        );
-      }
-    }
-    return dp[str1.length()][str2.length()];
-  }
-
-
 }
