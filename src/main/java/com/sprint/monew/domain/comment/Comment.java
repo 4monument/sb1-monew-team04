74ba85d68d25416c785327f93e7cb88325ad58bf
Oince <hky0105@naver.com>
2025-04-25 19:27:32 +0900

    feat: 논리 삭제된 댓글을 수정하려하면 예외 발생

--- src/main/java/com/sprint/monew/domain/comment/Comment.java
+++ src/main/java/com/sprint/monew/domain/comment/Comment.java
@@ -1,6 +1,7 @@
 package com.sprint.monew.domain.comment;
 
 import com.sprint.monew.domain.article.Article;
+import com.sprint.monew.domain.comment.exception.CommentNotFoundException;
 import com.sprint.monew.domain.comment.like.Like;
 import com.sprint.monew.domain.user.User;
 import jakarta.persistence.CascadeType;
@@ -77,7 +78,7 @@ public class Comment {
 
   public void updateContent(String content) {
     if (this.deleted) {
-      // 삭제된 메세지인 경우 예외 발생. 추후 커스텀 예외 추가 예정
+      throw CommentNotFoundException.withId(this.id);
     }
     this.content = content;
   }
