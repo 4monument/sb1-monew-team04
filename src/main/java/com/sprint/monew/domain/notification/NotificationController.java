171145816b8f2aba26c394b5c5b145a3cce77726
yudility <yudility@gmail.com>
2025-04-28 15:36:08 +0900

    test: 알림 생성 테스트용 컨트롤러 메소드 임시 구현

--- src/main/java/com/sprint/monew/domain/notification/NotificationController.java
+++ src/main/java/com/sprint/monew/domain/notification/NotificationController.java
@@ -1,12 +1,16 @@
 package com.sprint.monew.domain.notification;
 
 import com.sprint.monew.common.util.CursorPageResponseDto;
+import java.time.Instant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -36,6 +40,13 @@ public class NotificationController {
   public ResponseEntity<?> checkNotification(@PathVariable String notificationId) {
     //Monew-Request-User-ID는 헤더로 받음
     return null;
+  }
+
+  //테스트용 생성
+  @PostMapping
+  public ResponseEntity<List<Notification>> createNotification(
+      @RequestParam Instant afterAt) {
+    return ResponseEntity.ok(notificationService.createArticleInterestNotifications(afterAt));
   }
 
 }
