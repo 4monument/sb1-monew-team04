3009dd9f3c5a503acb8223e7a59d47764c8e610c
yudility <yudility@gmail.com>
2025-04-29 09:26:17 +0900

    fix: 알림 생성 로직 Processor에 맞게 수정

--- src/main/java/com/sprint/monew/domain/notification/NotificationController.java
+++ src/main/java/com/sprint/monew/domain/notification/NotificationController.java
@@ -1,16 +1,12 @@
 package com.sprint.monew.domain.notification;
 
 import com.sprint.monew.common.util.CursorPageResponseDto;
-import java.time.Instant;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -42,11 +38,5 @@ public class NotificationController {
     return null;
   }
 
-  //테스트용 생성
-  @PostMapping
-  public ResponseEntity<List<Notification>> createNotification(
-      @RequestParam Instant afterAt) {
-    return ResponseEntity.ok(notificationService.createArticleInterestNotifications(afterAt));
-  }
 
 }
