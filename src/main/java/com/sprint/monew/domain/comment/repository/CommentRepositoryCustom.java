01119596f30b37a5914e1e8eafdedbe43a756a81
Oince <hky0105@naver.com>
2025-04-30 22:50:49 +0900

    fix: getComments() 쿼리 오류 수정

--- src/main/java/com/sprint/monew/domain/comment/repository/CommentRepositoryCustom.java
+++ src/main/java/com/sprint/monew/domain/comment/repository/CommentRepositoryCustom.java
@@ -3,11 +3,11 @@ package com.sprint.monew.domain.comment.repository;
 import com.sprint.monew.domain.comment.dto.CommentDto;
 import com.sprint.monew.domain.comment.dto.request.CommentRequest;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface CommentRepositoryCustom {
 
-  Page<CommentDto> getComments(CommentRequest condition, UUID userId, Pageable pageable);
+  Slice<CommentDto> getComments(CommentRequest condition, UUID userId, Pageable pageable);
 
 }
