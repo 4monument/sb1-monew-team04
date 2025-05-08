0b949d1e2d5290419a82ff0b5ea081087c21e5ec
yudility <yudility@gmail.com>
2025-05-08 16:15:34 +0900

    fix: UserUpdateRequest Dto nickname 데이터 유효성 검증 오류 수정

--- src/main/java/com/sprint/monew/domain/user/UserUpdateRequest.java
+++ src/main/java/com/sprint/monew/domain/user/UserUpdateRequest.java
@@ -1,11 +1,11 @@
 package com.sprint.monew.domain.user;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
     @NotBlank
-    @Max(50)
+    @Size(min = 1, max = 50)
     String nickname
 ) {
 
