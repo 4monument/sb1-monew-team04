3bd31e5770853a9de6b0687eb2a3f485d47aa8b2
yudility <yudility@gmail.com>
2025-05-09 09:27:46 +0900

    fix: 관심사 생성 및 수정 유효성 검증 누락 수정 및 구독 중복 오류 수정

--- src/main/java/com/sprint/monew/domain/interest/dto/InterestCreateRequest.java
+++ src/main/java/com/sprint/monew/domain/interest/dto/InterestCreateRequest.java
@@ -1,6 +1,7 @@
 package com.sprint.monew.domain.interest.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
@@ -11,7 +12,7 @@ public record InterestCreateRequest(
 
     @Schema(description = "관련 키워드 목록")
     @Size(min = 1, max = 10)
-    List<String> keywords
+    List<@NotBlank @Size(min = 1, max = 20) String> keywords
 ) {
 
 }
